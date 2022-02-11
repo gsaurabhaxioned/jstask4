ee7ea16ece41498a750dd462cb4718155dcc7495
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-11 15:55:17 +0530

    javascript code for searchbar started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-/* Author: 
+let search = document.querySelectorAll(".searchbutton"),
 
-*/
+searchname = document.querySelectorAll('.searchname');
 
-
-
-
-
-
-
-
+search[0].addEventListener('click',function(){
+    if(searchname[0].getElementsByClassName.width=="auto"){
+        searchname[0].getElementsByClassName.width="0%";
+    }else{
+        searchname[0].getElementsByClassName.width="auto";
+    }
+    
+});
 
 
 
