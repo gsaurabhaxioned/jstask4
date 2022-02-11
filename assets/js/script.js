3c79fb46c8b5f4c1023487ad875185e78ee19d7c
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-11 16:14:26 +0530

    javascript code completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,7 @@
 let search = document.querySelectorAll(".searchbutton"),
 
-searchname = document.querySelectorAll('.searchname');
+    searchname = document.querySelectorAll('.searchname');
 
-search[0].addEventListener('click',function(){
-    if(searchname[0].getElementsByClassName.width=="auto"){
-        searchname[0].getElementsByClassName.width="0%";
-    }else{
-        searchname[0].getElementsByClassName.width="auto";
-    }
-    
+search[0].addEventListener('click', function () {
+    searchname[0].classList.toggle('searchname-hide');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
