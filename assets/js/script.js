37f3bf6f723e016f513436784e0c73ef6f3d9064
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 19:29:48 +0530

    js code animation duration value changed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,9 @@
 let search = document.querySelector(".searchbutton"),
 
-    searchname = document.querySelector('.searchname');
+    searchname = document.querySelector('.searchname'),
     searchname_hide = document.querySelector('.searchname-hide');
 
 search.addEventListener('click', function () {
-    searchname.classList.add('searchname-hide');
-    searchname.style.animationDuration="0.5s";
+    searchname.classList.toggle('searchname-hide');
+    searchname.style.animationDuration = "0.3s";
 });
-
-
-
