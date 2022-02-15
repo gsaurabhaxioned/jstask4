ac5a5d34ab0ea74e5a78fca708f6cde32c3bf0b2
saurabh-gaonkar <saurabhg@axioned.com>
2022-02-15 19:23:02 +0530

    js code added for class toggling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 let search = document.querySelector(".searchbutton"),
 
     searchname = document.querySelector('.searchname');
+    searchname_hide = document.querySelector('.searchname-hide');
 
 search.addEventListener('click', function () {
-    searchname.classList.toggle('searchname-hide');
+    searchname.classList.add('searchname-hide');
+    searchname.style.animationDuration="0.5s";
 });
+
+
 
